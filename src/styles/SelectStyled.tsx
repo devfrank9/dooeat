6e878a8606ad08e7d056150ac28ae54fa961bd8f
leejongho <devfrank9@gmail.com>
2022-02-14 15:33:32 +0900

    select박스 몸통만 스타일링

--- src/styles/SelectStyled.tsx
+++ src/styles/SelectStyled.tsx
@@ -4,10 +4,20 @@ export const SelectTest = () => <Select></Select>;
 
 export const Select = styled.select`
   display: block;
-  min-width: 3.6875rem;
-  min-height: 3rem;
+  width: 26.5833rem;
+  height: 4rem;
   border: 0.1rem solid ${props => props.theme.mainColor};
   border-radius: 0.5rem;
-  background-color: rgb(252, 216, 224);
-  color: rgb(239, 144, 167);
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  background: url('image/Icon ionic-ios-arrow-up.png') no-repeat 95% 50%;
+  box-sizing: border-box;
+  background-color: rgb(255, 249, 249);
+  color: ${props => props.theme.mainColor};
+  text-align: center;
+  font-size: 1.25rem;
+  &::-ms-expand {
+    display: none;
+  }
 `;
