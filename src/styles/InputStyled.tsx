fcdc6f876a8d5e47d52be94a9d41463a4755cb0e
leejongho <devfrank9@gmail.com>
2022-02-14 15:33:14 +0900

    필요없는 render부분 주석처리, textarea 스타일 추가

--- src/styles/InputStyled.tsx
+++ src/styles/InputStyled.tsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
-
+/* 
 export const InputCommon = () => <Common />;
 export const InputCommonShort = () => <CommonShort />;
 export const InputCheckNone = () => <CheckNone />;
 export const InputCheckOk = () => <CheckOk />;
 export const InputCheckErr = () => <CheckErr />;
 export const InputContent = () => <Content />;
-
+ */
 export const Common = styled.input`
   width: 26.5833rem;
   height: 4.3333rem;
@@ -48,9 +48,7 @@ export const CommonShort = styled.input`
   }
 `;
 export const CheckNone = styled(Common)`
-  background-image: url('/image/Icon feather-check.png');
-  background-repeat: no-repeat;
-  background-position: 22.9167rem center;
+  background-image: url('/image/Icon feather-check.png') no-repeat 95% 50%;
   box-sizing: border-box;
 `;
 export const CheckOk = styled(CheckNone)`
@@ -63,5 +61,26 @@ export const Content = styled(Common)`
   p {
     position: absolute;
     z-index: 999px;
+  }
+`;
+export const TextArea = styled.input`
+  width: 26.5833rem;
+  height: 6.6667rem;
+  padding-left: 1.5rem;
+  border-radius: 0.5rem;
+  font-size: 1.25rem;
+  border: 0.1rem solid rgb(223, 223, 223);
+  display: block;
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  &::-webkit-input-placeholder {
+    font-size: 1.1667rem;
+    opacity: 0.5;
+  }
+  &:focus {
+    outline: none;
+  }
+  &:focus::-webkit-input-placeholder {
+    color: transparent;
   }
 `;
