4fca4181f90c4043551c3fd68d8b33c28deec4a1
leejongho <devfrank9@gmail.com>
2022-02-25 15:22:12 +0900

    필요없는 파일 삭제

--- src/screens/Comptest2.tsx
+++
@@ -1,8 +0,0 @@
-import react from 'react';
-
-interface Prop {
-  onChangeText: (e: string) => void;
-  inputStatus?: boolean;
-  compType: 'short' | 'content' | 'normal';
-}
-export default ({onChangeText, inputStatus, compType = 'normal'}: Prop) => {};
