187352ae532669726193e4617eabadd565ed9c6d
leejongho <devfrank9@gmail.com>
2022-03-04 19:33:01 +0900

    지역 더미 데이터 생성

--- src/lib/Dummys.tsx
+++ src/lib/Dummys.tsx
@@ -1,8 +1,21 @@
 export const DummySelect = [
-  {id: 1, name: '울릉도'},
-  {id: 2, name: '동남쪽'},
-  {id: 3, name: '뱃길따라'},
-  {id: 4, name: '이백리'},
+  {id: '2', region: '서울'},
+  {id: '3', region: '경기'},
+  {id: '4', region: '인천'},
+  {id: '5', region: '부산'},
+  {id: '6', region: '대전'},
+  {id: '7', region: '대구'},
+  {id: '8', region: '울산'},
+  {id: '9', region: '세종'},
+  {id: '10', region: '광주'},
+  {id: '11', region: '강원'},
+  {id: '12', region: '충북'},
+  {id: '13', region: '충남'},
+  {id: '14', region: '경북'},
+  {id: '15', region: '경남'},
+  {id: '16', region: '전북'},
+  {id: '17', region: '전남'},
+  {id: '18', region: '제주'},
 ];
 
 export const Chest = [
