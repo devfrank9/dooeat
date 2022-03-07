2eb01895bf4c30c873b7f8c22730242f196920e1
leejongho <devfrank9@gmail.com>
2022-03-07 19:48:30 +0900

    미리보기 스타일 코드 이동

--- src/screens/userMeal/Rectengle.tsx
+++ src/screens/userMeal/Rectengle.tsx
@@ -1,8 +1,23 @@
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
 
-export const Rectengle = () => {
-  return <RectLink to="/user/meal/edit"></RectLink>;
+interface Prop {
+  img: any;
+  emoji?: string;
+  text: string;
+}
+
+export const RectPrev = ({img, emoji, text}: Prop) => {
+  return (
+    <Preview to="/user/meal/edit" img={img}>
+      <div>{text}</div>
+      <img src={emoji} alt="" />
+    </Preview>
+  );
+};
+
+export const RectInput = () => {
+  return <RectLink to="/user/meal/edit" />;
 };
 
 const RectLink = styled(Link)`
@@ -14,5 +29,37 @@ const RectLink = styled(Link)`
   border-radius: 10px;
   background: url('/image/IIcon feather-plus-circle.png') no-repeat 50% 50%;
 `;
-
-export default Rectengle;
+const Preview = styled(Link)<{img: string}>`
+  position: relative;
+  display: block;
+  min-width: 150px;
+  width: 47.022%;
+  padding-bottom: 47.022%;
+  border: 1px solid ${props => props.theme.btnColor2};
+  border-radius: 10px;
+  background: ${prop => '../../Dummy/Image/' + prop.img + '.png'} no-repeat 50%
+    50%;
+  background-size: cover;
+  div {
+    position: absolute;
+    width: 68px;
+    height: 32px;
+    border-radius: 16px;
+    background-color: rgb(51, 51, 51, 0.6);
+    left: 12px;
+    top: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: ${prop => prop.theme.fontColor3};
+    font-size: 15px;
+    font-weight: 300;
+  }
+  img {
+    position: absolute;
+    width: 32px;
+    height: 32px;
+    right: 8px;
+    bottom: 8px;
+  }
+`;
