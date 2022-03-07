6dafb13c6533fb19009a3ce11ac22f3b4fb74453
leejongho <devfrank9@gmail.com>
2022-03-07 15:31:53 +0900

    컴포넌트에 state설정

--- src/screens/userMeal/MealMain.tsx
+++ src/screens/userMeal/MealMain.tsx
@@ -1,14 +1,28 @@
-import styled from 'styled-components';
 import BaseScreen, {AlignBase} from '../BaseScreen';
-
-import Rectengle from './Rectengle';
-import {WaterBlock} from '../../components/WaterBlock';
 import {StatusBar} from '../../components/StatusBar';
-import {SelectDate} from '../../components/SelectDate';
 import {Navigation} from '../../components/Navigation';
-import {Link} from 'react-router-dom';
+import DateSelect from '../../components/DateSelect';
+import * as Styled from '../../styles/userMeal/styled';
+import Rectengle from './Rectengle';
+import {useEffect, useState} from 'react';
+import {WaterBlock} from '../../components/WaterBlock';
+import {FoodData} from '../../Dummy/Dummy';
+import {useQueryForAllBoard} from '../../lib/GQL/CommunicationMap';
+import {useParams} from 'react-router-dom';
+import moment from 'moment';
 
-const mealMain = () => {
+const MealMain = () => {
+  const [getMoment, setMoment] = useState(moment());
+  const today = getMoment.format('YYYY. MM. DD');
+  const [water, setWater] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  const onClickWater = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setIsActive(prev => !prev);
+    if (isActive === true) setWater(water - 0.25);
+    else setWater(water + 0.25);
+  };
+
   return (
     <BaseScreen>
       <AlignBase>
@@ -17,84 +31,40 @@ const mealMain = () => {
           Img="/image/myInfo.png"
           LinkTo="/user/mypage"
         />
-        <div style={{height: '110px'}}></div>
-        <SelectDate />
-        <Subject>식단 입력</Subject>
-        <div
-          style={{
-            display: 'block',
-            justifyContent: 'space-between',
-            flexWrap: 'wrap',
-            width: '100%',
-            height: '30%',
+        <div style={{height: '110px'}} />
+        <DateSelect
+          today={today}
+          backClick={() => {
+            setMoment(getMoment.clone().subtract(1, 'day'));
           }}
-        >
-          <FileAlign>
+          forClick={() => {
+            setMoment(getMoment.clone().add(1, 'day'));
+          }}
+        />
+        <Styled.Subject>식단 입력</Styled.Subject>
+        <Styled.RectengleAlign>
+          <Styled.FileAlign>
             <Rectengle />
             <Rectengle />
             <Rectengle />
-            <Preview to="/user/meal/edit">
+            <Styled.Preview to="/user/meal/edit">
               <div>점심</div>
               <img src="/image/smile.png" alt="" />
-            </Preview>
-          </FileAlign>
-        </div>
+            </Styled.Preview>
+          </Styled.FileAlign>
+        </Styled.RectengleAlign>
         <div style={{height: '32px'}} />
-        <Subject>하루 물 섭취 권장량 2L</Subject>
-        <WaterBlock />
-        <div style={{flex: 1}}></div>
+        <Styled.Subject>하루 물 섭취 권장량 2L</Styled.Subject>
+        <WaterBlock
+          isActive={isActive}
+          onClickWater={onClickWater}
+          water={water}
+        />
         <div style={{height: '135px'}} />
         <Navigation />
       </AlignBase>
     </BaseScreen>
   );
 };
-const FileAlign = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  a {
-    margin-bottom: 12px;
-  }
-`;
-const Subject = styled.div`
-  font-size: 18px;
-  font-weight: 500;
-  margin-top: 32px;
-  margin-bottom: 12px;
-`;
-const Preview = styled(Link)`
-  position: relative;
-  display: block;
-  min-width: 150px;
-  width: 47.022%;
-  padding-bottom: 47.022%;
-  border: 1px solid ${props => props.theme.btnColor2};
-  border-radius: 10px;
-  background: url('/image/321.png') no-repeat 50% 50%;
-  background-size: cover;
-  div {
-    position: absolute;
-    width: 68px;
-    height: 32px;
-    border-radius: 16px;
-    background-color: rgb(51, 51, 51, 0.6);
-    left: 12px;
-    top: 12px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: ${prop => prop.theme.fontColor3};
-    font-size: 15px;
-    font-weight: 300;
-  }
-  img {
-    position: absolute;
-    width: 32px;
-    height: 32px;
-    right: 8px;
-    bottom: 8px;
-  }
-`;
 
-export default mealMain;
+export default MealMain;
