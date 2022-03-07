884b590b4858e0a9a02a10fccc241feff5ac6c2b
leejongho <devfrank9@gmail.com>
2022-03-07 19:49:49 +0900

    유저 식단관리 메인화면 data fetch

--- src/screens/userMeal/MealMain.tsx
+++ src/screens/userMeal/MealMain.tsx
@@ -3,25 +3,38 @@ import {StatusBar} from '../../components/StatusBar';
 import {Navigation} from '../../components/Navigation';
 import DateSelect from '../../components/DateSelect';
 import * as Styled from '../../styles/userMeal/styled';
-import Rectengle from './Rectengle';
 import {useEffect, useState} from 'react';
 import {WaterBlock} from '../../components/WaterBlock';
-import {FoodData} from '../../Dummy/Dummy';
-import {useQueryForAllBoard} from '../../lib/GQL/CommunicationMap';
-import {useParams} from 'react-router-dom';
 import moment from 'moment';
+import {RectPrev, RectInput} from './Rectengle';
+import {FoodData} from '../../Dummy/Dummy';
+import {ResponseQueryGetBoardListBoardList} from '../../lib/GQL/GQLInterfaces';
 
 const MealMain = () => {
   const [getMoment, setMoment] = useState(moment());
   const today = getMoment.format('YYYY. MM. DD');
   const [water, setWater] = useState(0);
   const [isActive, setIsActive] = useState(false);
+  const keyDate = getMoment.format('YYYY-MM-DD');
+  const [render, setRender] = useState(false);
+  const boardQuery = FoodData.getBoardList.boardList;
+  const [userData, setUserData] = useState<
+    ResponseQueryGetBoardListBoardList[]
+  >([]);
+
+  /*   for (let i = 0; i <= boardQuery.length; i++) {
+    if (boardQuery[i].datetime === keyDate) setRender(prev => !prev);
+  } */
 
   const onClickWater = (e: React.MouseEvent<HTMLButtonElement>) => {
     setIsActive(prev => !prev);
     if (isActive === true) setWater(water - 0.25);
     else setWater(water + 0.25);
   };
+  useEffect(() => {
+    setUserData(boardQuery);
+  }, [boardQuery, userData]);
+  useEffect(() => {}, [render]);
 
   return (
     <BaseScreen>
@@ -44,21 +57,17 @@ const MealMain = () => {
         <Styled.Subject>식단 입력</Styled.Subject>
         <Styled.RectengleAlign>
           <Styled.FileAlign>
-            <Rectengle />
-            <Rectengle />
-            <Rectengle />
-            <Styled.Preview to="/user/meal/edit">
-              <div>점심</div>
-              <img src="/image/smile.png" alt="" />
-            </Styled.Preview>
+            {userData.map(data => (
+              <RectPrev key={data.wr_id} img={data} text={data.wr_2} />
+            ))}
           </Styled.FileAlign>
         </Styled.RectengleAlign>
         <div style={{height: '32px'}} />
         <Styled.Subject>하루 물 섭취 권장량 2L</Styled.Subject>
         <WaterBlock
-          isActive={isActive}
-          onClickWater={onClickWater}
-          water={water}
+          active={isActive}
+          onClick={onClickWater}
+          waterCalc={water}
         />
         <div style={{height: '135px'}} />
         <Navigation />
