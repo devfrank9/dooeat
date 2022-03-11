18be151c4e676c6406dd567eaeb58393ee8575dd
leejongho <devfrank9@gmail.com>
2022-03-11 17:10:45 +0900

    일단은 date를 파라미터로 사용

--- src/screens/health/UserHealthRoute.tsx
+++ src/screens/health/UserHealthRoute.tsx
@@ -1,6 +1,5 @@
 import {Route, Routes} from 'react-router-dom';
 import HealthMain from './HealthMain';
-import HealthMain2 from './HealthMain2';
 import ScaleGraph from './ScaleGraph';
 import HealthEdit from './HealthEdit';
 
@@ -8,9 +7,9 @@ function SearchMento() {
   return (
     <Routes>
       <Route path="/" element={<HealthMain />} />
-      <Route path="2" element={<HealthMain2 />} />
       <Route path="scale-graph" element={<ScaleGraph />} />
       <Route path="edit" element={<HealthEdit />} />
+      <Route path=":date" element={<HealthEdit />} />
     </Routes>
   );
 }
