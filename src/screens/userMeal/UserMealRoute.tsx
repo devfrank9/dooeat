363e1fdd0f26ceb2035fea166fc6a3ce46c732ea
leejongho <devfrank9@gmail.com>
2022-03-15 17:37:45 +0900

    필요없는 코드

--- src/screens/userMeal/UserMealRoute.tsx
+++ src/screens/userMeal/UserMealRoute.tsx
@@ -2,29 +2,29 @@ import {Route, Routes} from 'react-router-dom';
 import MealMain from './MealMain';
 import MealWrite from './MealWrite';
 import MealEdit from './MealEdit';
-import {useRecoilState} from 'recoil';
-import {__me} from '../../lib/atom';
-import {useLQueryForGetMe} from '../../lib/GQL/CommunicationMap';
-import {useEffect} from 'react';
+import BaseScreen, {AlignBase} from '../BaseScreen';
+import {Navigation} from '../../components/Navigation';
 
 function UserMealRoute() {
-  const [loginData, loginDataResult] = useLQueryForGetMe();
-  const [getMe, setMe] = useRecoilState(__me);
-
-  useEffect(() => {
-    //@ts-ignore
-    loginData({variables: sessionStorage.getItem('currentLoginSession')});
-    if (loginDataResult.data && loginDataResult.data.getMe) {
-      setMe(loginDataResult.data.getMe);
-    }
-  }, [UserMealRoute]);
-
   return (
-    <Routes>
-      <Route path="/" element={<MealMain />} />
-      <Route path="write" element={<MealWrite />} />
-      <Route path="edit/:wr_id" element={<MealEdit />} />
-    </Routes>
+    <>
+      <BaseScreen>
+        <AlignBase>
+          <Routes>
+            <Route path="/" element={<MealMain />} />
+          </Routes>
+        </AlignBase>
+      </BaseScreen>
+      <BaseScreen>
+        <AlignBase>
+          <Routes>
+            <Route path="write" element={<MealWrite />} />
+            <Route path="edit/:wr_id" element={<MealEdit />} />
+          </Routes>
+          <Navigation />
+        </AlignBase>
+      </BaseScreen>
+    </>
   );
 }
 
