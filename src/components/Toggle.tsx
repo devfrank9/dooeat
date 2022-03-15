148ce1effcf8a4d95540f707ddb19f1476ac05f6
leejongho <devfrank9@gmail.com>
2022-03-15 15:59:16 +0900

    토글 컴포넌트

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,46 +1,98 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styled from 'styled-components';
+import useToggle from '../hooks/useToggle';
 
-const Switch = (props: any) => {
-  const {isChecked, handleToggle} = props;
-
+const Switch = ({label}: any) => {
+  const {isEnabled, toggleCallback} = useToggle();
   return (
-    <div style={{width: '100px', height: '100px'}}>
-      <SwitchCheckBox
-        type="checkbox"
-        checked={isChecked}
-        onChange={handleToggle}
-        id={`switch-input`}
-      />
-      <SwitchLabel htmlFor={`switch-input`}>
-        <Ball />
-      </SwitchLabel>
-    </div>
+    <Container>
+      {label}{' '}
+      <ToggleSwitch>
+        <CheckBox
+          type="checkbox"
+          name={label}
+          id={label}
+          isChecked={isEnabled === false ? false : true}
+          onChange={e => toggleCallback()}
+        />
+        <Label htmlFor={label} isChecked={isEnabled === false ? false : true}>
+          <Inner isChecked={isEnabled === false ? false : true} />
+          <SwitchSpan isChecked={isEnabled === false ? false : true} />
+        </Label>
+      </ToggleSwitch>
+    </Container>
   );
 };
 
-const SwitchCheckBox = styled.input`
+const ToggleSwitch = styled.div`
+  position: relative;
+  width: 62px;
+  display: inline-block;
+  text-align: left;
+  margin-left: 8px;
+`;
+const CheckBox = styled.input<{isChecked: boolean}>`
   display: none;
-  &:checked + .switch-label-theme .ball {
-    transform: translateX(24px);
+  margin-left: ${prop => (prop.isChecked === false ? `none` : 0)};
+  right: ${prop => (prop.isChecked === false ? `none` : 0)};
+`;
+const Label = styled.label<{isChecked: boolean}>`
+  display: block;
+  overflow: hidden;
+  cursor: pointer;
+  border: 0 solid #bbb;
+  border-radius: 15px;
+  margin-left: ${prop => (prop.isChecked === false ? `none` : 0)};
+  right: ${prop => (prop.isChecked === false ? 'none' : 0)};
+`;
+const Inner = styled.span<{isChecked: boolean}>`
+  display: block;
+  width: 200%;
+  margin-left: ${prop => (prop.isChecked === false ? `-100%` : 0)};
+  transition: margin 0.3s ease-in 0s;
+  &::before,
+  &::after {
+    float: left;
+    width: 50%;
+    height: 30px;
+    padding: 0;
+    line-height: 32px;
+    color: #fff;
+    font-family: roboto;
+    box-sizing: border-box;
+    font-size: 13px;
+  }
+  &::before {
+    content: 'ON';
+    padding-left: 10px;
+    background-color: ${prop => prop.theme.mainColor};
+    color: white;
+  }
+  &::after {
+    content: 'OFF';
+    padding-right: 10px;
+    background-color: rgb(245, 245, 245);
+    color: white;
+    text-align: right;
   }
 `;
-const SwitchLabel = styled.label`
-  position: relative;
-  height: 26px;
-  width: 50px;
-  border-radius: 50px;
-  background-color: var(--switch-bg-color);
-`;
-const Ball = styled.div`
-  position: absolute;
-  height: 22px;
+const SwitchSpan = styled.span<{isChecked: boolean}>`
+  display: block;
   width: 22px;
-  border-radius: 50%;
-  top: 2px;
-  left: 2px;
-  transition: transform 0.2s linear;
-  background-color: var(--ball-color);
+  margin: 5px;
+  background: white;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: ${prop => (prop.isChecked === false ? '30px' : 0)};
+  border: 0 solid #bbb;
+  border-radius: 11px;
+  transition: all 0.3s ease-in 0s;
+`;
+const Container = styled.div`
+  display: flex;
+  text-align: center;
+  font-size: 14px;
 `;
 
-export default Switch;
+export default React.memo(Switch);
