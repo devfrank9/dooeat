249737294d784a153a5e31b5f67fa0f886804d29
leejongho <devfrank9@gmail.com>
2022-03-15 17:53:17 +0900

    네비바 경로 따라 이미지 변경

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,56 +1,62 @@
 import styled from 'styled-components';
-import {Link, useNavigate, useParams} from 'react-router-dom';
-import {useRoutes} from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 import {useRecoilValue} from 'recoil';
 import {__me} from '../lib/atom';
 
 export const Navigation = () => {
   const getUserLevel = useRecoilValue(__me);
-  const match1 = useRoutes([{path: '/profile'}]);
-  const match2 = useRoutes([{path: '/manage'}]);
-  const match3 = useRoutes([{path: '/community/favor'}]);
-  const match4 = useRoutes([{path: '/shop'}]);
-  const match5 = useRoutes([{path: '/meal'}]);
-  const match6 = useRoutes([{path: '/health'}]);
-  const match7 = useRoutes([{path: '/pro'}]);
-  const match8 = useRoutes([{path: '/community/favor'}]);
-  const match9 = useRoutes([{path: '/shop'}]);
-  const {path} = useParams();
   const navigate = useNavigate();
+  const {pathname} = useLocation();
 
   return (
     <>
       {getUserLevel?.mb_level === 2 ? (
-        <NavContainer>
+        <NavContainer
+          onClick={() => {
+            console.log(pathname);
+          }}
+        >
           <button onClick={() => navigate('/meal')}>
             <Img1
               src={
-                match5 !== undefined ? '/image/nav3.png' : '/image/nav3_un.png'
+                pathname === '/meal' ? '/image/nav3.png' : '/image/nav3_un.png'
               }
               alt=""
             />
           </button>
           <button onClick={() => navigate('/health')}>
             <Img1
-              src={match6 !== null ? '/image/nav4.png' : '/image/nav4_un.png'}
+              src={
+                pathname === '/health'
+                  ? '/image/nav4.png'
+                  : '/image/nav4_un.png'
+              }
               alt=""
             />
           </button>
           <button onClick={() => navigate('/pro')}>
             <Img1
-              src={match7 !== null ? '/image/nav5.png' : '/image/nav5_un.png'}
+              src={
+                pathname === '/pro' ? '/image/nav5.png' : '/image/nav5_un.png'
+              }
               alt=""
             />
           </button>
           <button onClick={() => navigate('/community/favor')}>
             <Img1
-              src={match8 !== null ? '/image/nav6.png' : '/image/nav6_un.png'}
+              src={
+                pathname === '/community/favor'
+                  ? '/image/nav6.png'
+                  : '/image/nav6_un.png'
+              }
               alt=""
             />
           </button>
           <button onClick={() => navigate('/shop')}>
             <Img1
-              src={match9 !== null ? '/image/nav7.png' : '/image/nav7_un.png'}
+              src={
+                pathname === '/shop' ? '/image/nav7.png' : '/image/nav7_un.png'
+              }
               alt=""
             />
           </button>
@@ -59,13 +65,21 @@ export const Navigation = () => {
         <NavContainer>
           <button style={{width: '94px'}} onClick={() => navigate('/profile')}>
             <Img1
-              src={match1 !== null ? '/image/nav2.png' : '/image/nav2_un.png'}
+              src={
+                pathname === '/profile'
+                  ? '/image/nav2.png'
+                  : '/image/nav2_un.png'
+              }
               alt=""
             />
           </button>
           <button style={{width: '94px'}} onClick={() => navigate('/manage')}>
             <Img1
-              src={match2 !== null ? '/image/nav1.png' : '/image/nav1_un.png'}
+              src={
+                pathname === '/manage'
+                  ? '/image/nav1.png'
+                  : '/image/nav1_un.png'
+              }
               alt=""
             />
           </button>
@@ -74,13 +88,19 @@ export const Navigation = () => {
             onClick={() => navigate('/community/favor')}
           >
             <Img1
-              src={match3 !== null ? '/image/nav6.png' : '/image/nav6_un.png'}
+              src={
+                pathname === '/community/favor'
+                  ? '/image/nav6.png'
+                  : '/image/nav6_un.png'
+              }
               alt=""
             />
           </button>
           <button style={{width: '94px'}} onClick={() => navigate('/shop')}>
             <Img1
-              src={match4 !== null ? '/image/nav7.png' : '/image/nav7_un.png'}
+              src={
+                pathname === '/shop' ? '/image/nav7.png' : '/image/nav7_un.png'
+              }
               alt=""
             />
           </button>
