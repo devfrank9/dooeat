0df3b44ff4be374b879f3b84343b9f4c57754562
leejongho <devfrank9@gmail.com>
2022-03-18 14:06:14 +0900

    로딩스크린 에러 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import {useEffect} from 'react';
 import './styles/App.css';
 import {BrowserRouter} from 'react-router-dom';
 import {
@@ -6,14 +7,17 @@ import {
   createHttpLink,
   InMemoryCache,
 } from '@apollo/client';
-import {RecoilRoot} from 'recoil';
+import {RecoilRoot, useRecoilState, useSetRecoilState} from 'recoil';
 import {createNetworkStatusNotifier} from 'react-apollo-network-status';
 import Constants from './lib/Constants';
+import {__isLoading, __session} from './lib/atom';
+import {useLocalStorage, useSessionStorage} from 'react-use-storage';
 import {GlobalStyle} from './styles/GlobalStyle';
 import Router from './Router';
-import ScreenHooker from './lib/ScreenHooker';
+import LoadingScreen from './screens/includes/LoadingScreen';
 
-const {link} = createNetworkStatusNotifier();
+const {link, useApolloNetworkStatus} = createNetworkStatusNotifier();
+
 const appClient = new ApolloClient({
   cache: new InMemoryCache(),
   defaultOptions: {
@@ -36,6 +40,50 @@ const appClient = new ApolloClient({
     createHttpLink({uri: Constants.urls.gqlServer, credentials: 'include'}),
   ),
 });
+
+const ScreenHooker = () => {
+  const [value] = useLocalStorage(Constants.storage.autoLoginSession, '');
+  const [sessionValue] = useSessionStorage(
+    Constants.storage.currentLoginSession,
+    '',
+  );
+  const status = useApolloNetworkStatus();
+  const [isLoading, setIsLoading] = useRecoilState(__isLoading);
+  const setSession = useSetRecoilState(__session);
+
+  useEffect(() => {
+    if (sessionValue) {
+      setSession(sessionValue);
+    } else if (value) {
+      setSession(value);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (status.numPendingQueries > 0 || status.numPendingMutations > 0) {
+      setIsLoading(true);
+    } else {
+      if (
+        status.queryError &&
+        status.queryError.operation.operationName !== 'checkDuplicate'
+      ) {
+        let error = status.queryError
+          ? status.queryError
+          : status.mutationError;
+        if (error) {
+          if (error.networkError) {
+            alert(error.networkError.message);
+          } else if (error.graphQLErrors) {
+            alert(error.graphQLErrors[0].message);
+          }
+        }
+      }
+      setIsLoading(false);
+    }
+  }, [status]);
+
+  return isLoading ? <LoadingScreen /> : null;
+};
 
 const App = () => {
   return (
