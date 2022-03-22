e4b5b931911e634bdd5a052e6bd35af90ec600eb
leejongho <devfrank9@gmail.com>
2022-03-22 18:26:10 +0900

    라우팅수정

--- src/screens/userMeal/UserMealRoute.tsx
+++ src/screens/userMeal/UserMealRoute.tsx
@@ -13,7 +13,7 @@ function UserMealRoute() {
         <Routes>
           <Route path="*" element={<MealMain />} />
           <Route path="/write" element={<MealWrite />} />
-          <Route path=":wr_id" element={<MealWrite />} />
+          <Route path=":wr_1/:wr_id" element={<MealWrite />} />
         </Routes>
         {pathname === '/' ? <Navigation /> : <></>}
       </AlignBase>
