ebf56ad94a8ddec4919319b501c4c38ec6b70ffa
leejongho <devfrank9@gmail.com>
2022-03-24 16:01:20 +0900

    조금 이상하지만 버튼 에러 수정

--- src/components/MealSelecBtn.tsx
+++ src/components/MealSelecBtn.tsx
@@ -11,9 +11,10 @@ interface Iselect {
 }
 interface Prop {
   sendChild?: string[];
+  toggleModal: () => void;
 }
 
-const MealSelecBtn = ({sendChild}: Prop) => {
+const MealSelecBtn = ({sendChild, toggleModal}: Prop) => {
   const arr = [
     {
       key: 1,
@@ -30,10 +31,9 @@ const MealSelecBtn = ({sendChild}: Prop) => {
   ];
   const [pick, setPick] = useState<Iselect[]>(arr);
   const [select, setSelect] = useState<string[]>([]);
-  const selectChange = (item: string) => {
-    if (!select.includes(item)) return setSelect(select => [...select, item]);
-    else return setSelect(select.filter(button => button !== item));
-  };
+  const [select2, setSelect2] = useState<string[]>([]);
+  const [select3, setSelect3] = useState<string[]>([]);
+
   const [mealData, setMealData] = useRecoilState(mealBtnState);
 
   const btnRender = (n: number) => {
@@ -44,7 +44,9 @@ const MealSelecBtn = ({sendChild}: Prop) => {
           key={index}
           name="wr_5"
           onClick={() => {
-            selectChange(item);
+            !select.includes(item)
+              ? setSelect(select => [...select, item])
+              : setSelect(select.filter(button => button !== item));
             setMealData({...mealData, wr_5: item});
           }}
           isActive={
@@ -61,11 +63,13 @@ const MealSelecBtn = ({sendChild}: Prop) => {
           key={index}
           name="wr_2"
           onClick={() => {
-            selectChange(item);
+            !select2.includes(item)
+              ? setSelect2(select2 => [item])
+              : setSelect2(select2.filter(button => button !== item));
             setMealData({...mealData, wr_2: item});
           }}
           isActive={
-            select.includes(item) || sendChild?.includes(item) ? true : false
+            select2.includes(item) || sendChild?.includes(item) ? true : false
           }
         >
           {item}
@@ -78,11 +82,13 @@ const MealSelecBtn = ({sendChild}: Prop) => {
           key={index}
           name="wr_3"
           onClick={() => {
-            selectChange(item);
+            !select3.includes(item)
+              ? setSelect3(select3 => [item])
+              : setSelect3(select3.filter(button => button !== item));
             setMealData({...mealData, wr_3: item});
           }}
           isActive={
-            select.includes(item) || sendChild?.includes(item) ? true : false
+            select3.includes(item) || sendChild?.includes(item) ? true : false
           }
         >
           {item}
@@ -97,12 +103,27 @@ const MealSelecBtn = ({sendChild}: Prop) => {
       <BtnAlign>{btnRender(0)}</BtnAlign>
       <Subject>먹은 양</Subject>
       <BtnAlign>{btnRender(1)}</BtnAlign>
-      <Subject>식단 종류</Subject>
+      <SubSubject>
+        <Subject>식단 종류</Subject>
+        <MealBtn onClick={() => toggleModal()} />
+      </SubSubject>
       <BtnAlign>{btnRender(2)}</BtnAlign>
     </>
   );
 };
 
+const MealBtn = styled.button`
+  width: 20px;
+  height: 20px;
+  background: url('/image/group 47.png') no-repeat 50% 50%;
+  border: none;
+  border-radius: 10px;
+`;
+const SubSubject = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 const BtnAlign = styled.div`
   display: flex;
   flex-wrap: wrap;
