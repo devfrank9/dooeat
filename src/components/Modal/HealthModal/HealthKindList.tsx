d6330ee64ec6d1f3003216d7ff8a99011e400eeb
leejongho <devfrank9@gmail.com>
2022-03-24 14:58:42 +0900

    헬스 데이터 이동

---
+++ src/components/Modal/HealthModal/HealthKindList.tsx
@@ -0,0 +1,180 @@
+export interface IExKindSelect {
+  map(arg0: (star: any) => any): import('react').SetStateAction<IExKindSelect>;
+  [index: number]: {
+    id: number;
+    content: string;
+    active: boolean;
+  };
+}
+
+export const Leg = [
+  {
+    id: 1,
+    content: '스쿼트',
+    active: false,
+  },
+  {
+    id: 2,
+    content: '런지',
+    active: false,
+  },
+  {
+    id: 3,
+    content: '백 익스텐션',
+    active: false,
+  },
+  {
+    id: 4,
+    content: '레그 컬',
+    active: false,
+  },
+];
+export const Chest = [
+  {
+    id: 1,
+    content: '벤치프레스',
+    active: false,
+  },
+  {
+    id: 2,
+    content: '푸쉬업',
+    active: false,
+  },
+  {
+    id: 3,
+    content: '딥스',
+    active: false,
+  },
+  {
+    id: 4,
+    content: '덤벨 플라이',
+    active: false,
+  },
+];
+export const Shoulder = [
+  {
+    id: 1,
+    content: '바벨 프레스',
+    active: false,
+  },
+  {
+    id: 2,
+    content: '레터럴 레이즈',
+    active: false,
+  },
+  {
+    id: 3,
+    content: '풀업',
+    active: false,
+  },
+  {
+    id: 4,
+    content: '윈암 덤벨 로우',
+    active: false,
+  },
+];
+export const Arm = [
+  {
+    id: 1,
+    content: '바벨 컬',
+    active: false,
+  },
+  {
+    id: 2,
+    content: '덤벨 컬',
+    active: false,
+  },
+  {
+    id: 3,
+    content: '케이블 푸시다운',
+    active: false,
+  },
+  {
+    id: 4,
+    content: '킥백',
+    active: false,
+  },
+];
+export const Back = [
+  {
+    id: 1,
+    content: '랫풀다운',
+    active: false,
+  },
+  {
+    id: 2,
+    content: '친업',
+    active: false,
+  },
+  {
+    id: 3,
+    content: '데드리프트',
+    active: false,
+  },
+  {
+    id: 4,
+    content: '시티드 로우',
+    active: false,
+  },
+];
+export const Body = [
+  {
+    id: 1,
+    content: '플랭크',
+    active: false,
+  },
+  {
+    id: 2,
+    content: '버피',
+    active: false,
+  },
+  {
+    id: 3,
+    content: '런지',
+    active: false,
+  },
+  {
+    id: 4,
+    content: '러닝',
+    active: false,
+  },
+];
+export const Running = [
+  {
+    id: 1,
+    content: '러닝',
+    active: false,
+  },
+  {
+    id: 2,
+    content: '자전거',
+    active: false,
+  },
+  {
+    id: 3,
+    content: '수영',
+    active: false,
+  },
+];
+export const Stretch = [
+  {
+    id: 1,
+    content: '준비',
+    active: false,
+  },
+  {
+    id: 2,
+    content: '시작',
+    active: false,
+  },
+  {
+    id: 3,
+    content: '안녕',
+    active: false,
+  },
+  {
+    id: 4,
+    content: '하이',
+    active: false,
+  },
+];
